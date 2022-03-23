5bc79c11f323a63688d58232c9265ffb0272db3d
FABRICIO FILHO <06325934903@educacional.up>
2022-03-23 12:07:36 -0300

    randomvetor finalizado

--- Fabricio/Desenvolvimento de Software/Aula-2/randomvetor.java
+++ Fabricio/Desenvolvimento de Software/Aula-2/randomvetor.java
@@ -3,22 +3,43 @@ import java.util.Scanner;
 
 public class randomvetor {
     public static void main(String args[]) {
-        int i, j, x;
-        int[] vet = new int [20];
+        int i, j, x, n;
+        int[] vet = new int[20];
+        boolean is, teste;
         Random random = new Random();
         Scanner input = new Scanner(System.in);
         for (i = 0; i < 20; i++) {
+            teste = false;
             x = 1 + random.nextInt(100);
-            for (j = 0; j < vet.length; j++) {
+            for (j = 0; j < 20; j++) {
                 if (x == vet[j]) {
-                    i--;
-                }
-                else {
-                    vet[i] = x;
+                    teste = true;
+                    break;
                 }
             }
-            System.out.println(vet[i]);
+            if (!teste == true) {
+                vet[i] = x;
+                System.out.println(vet[i]);
+            } else {
+                i--;
+            }
         }
+
+        System.out.print("Digite um numero de 1 a 100: ");
+        n = input.nextInt();
+        is = false;
+        for (i = 0; i < 20; i++) {
+            if (n == vet[i]) {
+                is = true;
+                break;
+            }
+        }
+        if (is) {
+            System.out.println("O seu numero existe no index: " + i);
+        } else {
+            System.out.println("O seu numero não existe no vetor");
+        }
+
         input.close();
 
     }
