6ecfdb2d7f53b4318eb0966bb7434722bd26b75c
FABRICIO FILHO <06325934903@educacional.up>
2022-05-11 11:53:42 -0300

    genius

--- Fabricio/Desenvolvimento de Software/aula-10/grafica/FrmGenius.java
+++ Fabricio/Desenvolvimento de Software/aula-10/grafica/FrmGenius.java
@@ -2,38 +2,90 @@ package grafica;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import logica.Genius;
+import logica.Genius.Cor;
 
 import java.awt.GridLayout;
 import java.awt.Dimension;
 import java.awt.Color;
 
 public class FrmGenius extends JFrame {
+
+    private Genius logicaGenius;
+
+    private JButton red;
+    private JButton green;
+    private JButton blue;
+    private JButton yellow;
+
     public FrmGenius() {
+        this.logicaGenius = new Genius();
+
         this.setLayout(new GridLayout(2, 2, 10, 10));
 
-        JButton btn = new JButton();
-        btn.setMinimumSize(new Dimension(50, 50));
-        btn.setBackground(Color.RED);
-        this.add(btn);
+        red = new JButton();
+        red.setMinimumSize(new Dimension(50, 50));
+        red.setBackground(Color.RED);
+        this.add(red);
 
-        btn = new JButton();
-        btn.setMinimumSize(new Dimension(50, 50));
-        btn.setBackground(Color.GREEN);
-        this.add(btn);
+        green = new JButton();
+        green.setMinimumSize(new Dimension(50, 50));
+        green.setBackground(Color.GREEN);
+        this.add(green);
 
-        btn = new JButton();
-        btn.setMinimumSize(new Dimension(50, 50));
-        btn.setBackground(Color.BLUE);
-        this.add(btn);
+        blue = new JButton();
+        blue.setMinimumSize(new Dimension(50, 50));
+        blue.setBackground(Color.BLUE);
+        this.add(blue);
 
-        btn = new JButton();
-        btn.setMinimumSize(new Dimension(50, 50));
-        btn.setBackground(Color.YELLOW);
-        this.add(btn);
+        yellow = new JButton();
+        yellow.setMinimumSize(new Dimension(50, 50));
+        yellow.setBackground(Color.YELLOW);
+        this.add(yellow);
 
         this.pack();
         this.setSize(600, 600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void animarBotoes() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(500);
+                    for (Cor cor : logicaGenius.getCoresSorteadas()) {
+                        switch (cor) {
+                            case VERMELHO:
+                                red.setBackground(Color.GRAY);
+                                Thread.sleep(500);
+                                red.setBackground(Color.RED);
+                                break;
+                            case VERDE:
+                                green.setBackground(Color.GRAY);
+                                Thread.sleep(500);
+                                green.setBackground(Color.GREEN);
+                                break;
+                            case AZUL:
+                                blue.setBackground(Color.GRAY);
+                                Thread.sleep(500);
+                                blue.setBackground(Color.BLUE);
+                                break;
+                            case AMARELO:
+                                yellow.setBackground(Color.GRAY);
+                                Thread.sleep(500);
+                                yellow.setBackground(Color.YELLOW);
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        };
     }
 }
--- Fabricio/Desenvolvimento de Software/aula-10/logica/Genius.java
+++ Fabricio/Desenvolvimento de Software/aula-10/logica/Genius.java
@@ -1,10 +1,58 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Genius {
-    public enum COR {
+    public enum Cor {
         VERMELHO,
         VERDE,
         AZUL,
         AMARELO
-    } 
+    }
+
+    private ArrayList<Cor> coresSorteadas;
+    private ArrayList<Cor> coresJogador;
+
+    public Genius() {
+        coresSorteadas = new ArrayList<Cor>();
+        coresJogador = new ArrayList<Cor>();
+    }
+
+    public ArrayList<Cor> getCoresSorteadas() {
+        return coresSorteadas;
+    }
+
+    public ArrayList<Cor> getCoresJogador() {
+        return coresJogador;
+    }
+
+    public void sortearCor() {
+        Random rnd = new Random();
+        int i = rnd.nextInt(4);
+        Cor corSorteada = Cor.values()[i];
+        coresSorteadas.add(corSorteada);
+    }
+
+    public void lerCor(Cor corJogador) {
+        coresJogador.add(corJogador);
+    }
+
+    public boolean compararCores() {
+        for (int i = 0; i < coresJogador.size(); i++) {
+            if (coresSorteadas.get(i) != coresJogador.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void limparListaJogador() {
+        this.coresJogador.clear();
+    }
+
+    public void gameOver() {
+        limparListaJogador();
+        this.coresSorteadas.clear();
+    }
 }
