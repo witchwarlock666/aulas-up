ec86fbe7351a0d08287ddd186129a778ae480886
Felpasw <felipecavalcantelacerda@hotmail.com>
2022-05-13 10:56:38 -0300

    javascriptfoda

---
+++ Felipe/Desenvolvimento Web/Aula JS/scripts.js
@@ -0,0 +1,6 @@
+function teste(){
+    document.getElementById("info").innerHTML = "Esse é um texto foda em JavaScript!";
+}
+function teste2(){
+    document.getElementById("info").innerHTML = "Olá, Mundo!";
+}
