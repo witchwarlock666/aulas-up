2cefc4494e3c1a960ba305d230e5533a5d370be6
FELIPE LACERDA <10671086960@educacional.up>
2022-05-20 09:27:43 -0300

    Atividades JS adicionadas

---
+++ Felipe/Desenvolvimento Web/Aula JS 2/Atividades JS/EX 3 Concluída/scripts.js
@@ -0,0 +1,6 @@
+function bagunca(){
+    let numeroUsuario = parseInt(prompt("Digite o número desejado"));
+    const anterior = numeroUsuario - 1;
+    const proximo = numeroUsuario + 1;
+    alert("Seu número digitado foi:" + numeroUsuario + "\n O número subsequente é " + proximo +" \n e o número anterior é " + anterior) 
+}
